d982838b2f4f203c361d4a2bbc5ec85a126981bc
olyaprykhodko <o.u.prihodko@gmail.com>
2026-04-13 22:56:49 +0300

    feat(server): update ESLint configuration, add validation pipe, and modify dependencies

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import fs from 'node:fs';
 
 async function bootstrap() {
+  fs.mkdirSync('./data/tmp', { recursive: true });
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  await app.listen(process.env.PORT ?? 3500);
 }
 bootstrap();
