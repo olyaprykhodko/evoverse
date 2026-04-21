df8992ac2fe0d675cfaed28cd77e9ca1036eb8ed
olyaprykhodko <o.u.prihodko@gmail.com>
2026-04-21 13:35:25 +0300

    feat: configure ServeStaticModule to serve client build files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
 import { StorageModule } from './storage/storage.module';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [StorageModule, UsersModule, FilesModule],
-  controllers: [AppController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'build'),
+      exclude: ['/users/{*path}', '/files/{*path}'],
+    }),
+    StorageModule,
+    UsersModule,
+    FilesModule,
+  ],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
