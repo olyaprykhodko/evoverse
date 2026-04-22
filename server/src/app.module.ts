16ebe6df377e616f0abb74d52ba922ba13614009
olyaprykhodko <o.u.prihodko@gmail.com>
2026-04-22 15:51:43 +0300

    feat: update app module to include Auth and Admin modules, and expand excluded paths in ServeStaticModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,16 +4,25 @@ import { join } from 'node:path';
 import { StorageModule } from './storage/storage.module';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client', 'build'),
-      exclude: ['/users/{*path}', '/files/{*path}'],
+      exclude: [
+        '/users/{*path}',
+        '/files/{*path}',
+        '/auth/{*path}',
+        '/admin/{*path}',
+      ],
     }),
     StorageModule,
     UsersModule,
     FilesModule,
+    AuthModule,
+    AdminModule,
   ],
   controllers: [],
   providers: [],
