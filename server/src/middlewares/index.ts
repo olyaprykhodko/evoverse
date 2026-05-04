80022e70fa970f028caf8268fef41707eaf87b89
Olha Prykhodko <o.u.prihodko@gmail.com>
2026-05-04 11:53:38 +0300

    project init

--- server/src/middlewares/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './error-handler.js';
-export * from './validate.js';
-export * from './logger.js';
--- server/src/modules/healthcheck/healthcheck.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { healthcheckController } from './healthcheck.controller.js';
-
-const router = Router();
-
-router.get('/health', healthcheckController.getHealth);
-
-export default router;
