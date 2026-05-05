a3f4957c8678afb9fbfe56c54e42d81163a0c092
Olha Prykhodko <o.u.prihodko@gmail.com>
2026-05-05 14:32:44 +0300

    fix: update default port to 3300

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3300);
 }
 await bootstrap();
