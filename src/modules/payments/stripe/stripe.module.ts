9774c1ef35abc2fc65e0eb54d42a34fb036f93ef
Olha Prykhodko <o.u.prihodko@gmail.com>
2026-05-26 15:50:08 +0300

    refactor: remove PaymentsController and update PaymentsModule; enhance PaymentsService and StripeService for checkout session handling

--- src/modules/payments/stripe/stripe.module.ts
+++ src/modules/payments/stripe/stripe.module.ts
@@ -3,8 +3,10 @@ import Stripe from 'stripe';
 import { StripeService } from './stripe.service.js';
 import { StripeController } from './stripe.controller.js';
 import { STRIPE_CLIENT } from './constants/client.js';
+import { PaymentsModule } from '../payments.module.js';
 
 @Module({
+  imports: [PaymentsModule],
   controllers: [StripeController],
   providers: [
     StripeService,
